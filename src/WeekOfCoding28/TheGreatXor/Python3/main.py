8768be2d897dae203fb9a444ba4fd84a5ce42ba3
Witkowski <matthew.witkowski@dev0043u12.g3ti.local>
2017-01-11 18:30:35 -0500

    Updated the solution after finding a pattern. Should be a lot faster now.

--- src/WeekOfCoding28/TheGreatXor/Python3/main.py
+++ src/WeekOfCoding28/TheGreatXor/Python3/main.py
@@ -1,26 +1,19 @@
-def meets_criteria(value, maximum):
-  return (value ^ maximum) > maximum
-
-
-def count_match_criteria(maximum, cache):
-  counter = 0
-  if maximum not in cache:
-    for value in range(maximum - 1):
-      if meets_criteria(value + 1, maximum):
-        counter += 1
-      cache[maximum] = counter
-  else:
-    counter = cache[maximum]
-  return counter
-
+def find_closest_value(number):
+  power = 0
+  while(2**power < number):
+    power += 1
+  return 2**power
 
 def main():
   lines = int(input())
-  cache = {}
   countList = []
   for line in range(lines):
-    maximum = int(input())
-    countList.append(str(count_match_criteria(maximum, cache)))
+    query = int(input()) 
+    closest = find_closest_value(query)
+    value = closest - 1
+    if(closest != query):
+      value -= query
+    countList.append(str(value))
   print("\n".join(countList))
 
 main()
